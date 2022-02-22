9caff63be9743135efabae2da30f5465af923581
yagamidsa <david.alipio@kantar.com>
2022-02-22 00:27:51 -0500

    deploymentheroku2

--- portafolioximena/settings.py
+++ portafolioximena/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'portafolioximena.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR , "/apps/experiencia/templates/"),],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
