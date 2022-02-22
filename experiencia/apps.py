a5bb5f55ebf518390a96ff354c041fd1b97fc676
yagamidsa <david.alipio@kantar.com>
2022-02-21 22:59:44 -0500

    deploymentheroku2

--- experiencia/apps.py
+++ experiencia/apps.py
@@ -4,5 +4,3 @@ from django.apps import AppConfig
 class ExperienciaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'experiencia'
-
-default_app_config = 'experiencia.apps.ExperienciaConfig'
