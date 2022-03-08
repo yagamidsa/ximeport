a6dcdd9338c5e420bffe6d379514a23aa45e8077
yagamidsa <david.alipio@kantar.com>
2022-03-08 17:51:43 -0500

    produccion final Prov2

--- portafolio/models.py
+++ portafolio/models.py
@@ -8,13 +8,13 @@ class Skill(models.Model):
     imagenlugar = models.ImageField(upload_to="")
     carrera = CharField(max_length=100)
     lugarcarrera = CharField(max_length=250)
-    descripcion = CharField(max_length=250)
+    descripcion = CharField(max_length=500)
 
 
 class Project(models.Model):
     Empresa = CharField(max_length=100, null=True, verbose_name='Empresa')
     Cargo = CharField(max_length=100,null=True, verbose_name='Cargo')
-    description = CharField(max_length=250,null=True, verbose_name='description')
+    description = CharField(max_length=500,null=True, verbose_name='description')
     url = URLField(blank=True)
     Web = URLField(blank=True)
     date = models.DateTimeField(datetime.date.today)
